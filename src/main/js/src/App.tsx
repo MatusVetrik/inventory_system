c3c76151c83f487a56ff223635ab5e33e0dee8f6
MatusVetrik <m.vetrik1@gmail.com>
2023-10-20 21:52:52 +0200

    feat: impl of appbar and navbar

--- src/main/js/src/App.tsx
+++ src/main/js/src/App.tsx
@@ -1,19 +1,8 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import routes from './routing/routes'
 import './App.scss'
+import routes from "./routing/routes";
 
 const App = () => (
     <>
-        <div>
-            <a href="https://vitejs.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo"/>
-            </a>
-            <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo"/>
-            </a>
-        </div>
-        <h1>Inventory system</h1>
         <div className="routes-list__wrapper">
             <h2>Routes</h2>
             <li>
--- src/main/js/src/main.tsx
+++ src/main/js/src/main.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { createRoot} from 'react-dom/client'
-import App from './App.tsx'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import Router from "./routing/router";
+import Layout from "./components/Layout";
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <Router/>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Layout>
+            <Router/>
+        </Layout>
+    </React.StrictMode>,
 )
