56e6f147459f49e9af3d8e7086277feaf9d6487e
Filip Mlýnek <mlynekff@gmail.com>
2023-10-25 22:02:29 +0200

    Remove redundant code

--- src/main/java/com/vetrikos/inventory/system/Application.java
+++ src/main/java/com/vetrikos/inventory/system/Application.java
@@ -1,29 +1,12 @@
 package com.vetrikos.inventory.system;
 
-import com.vetrikos.inventory.system.entity.User;
-import com.vetrikos.inventory.system.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class Application {
-    private final UserRepository userRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
     }
-
-    @Bean
-    public CommandLineRunner testFunction() {
-        return args -> {
-            userRepository.save(User.builder().username("meno").isManager(false).build());
-            System.out.println(userRepository.findAll());
-        };
-    }
-
 }
