9f9444a80007ea9f5d5f74446069911f4f055ac6
Filip Mlýnek <mlynekff@gmail.com>
2023-10-31 13:05:32 +0100

    Update entities with custom equals and hashcode

--- src/main/java/com/vetrikos/inventory/system/entity/Order.java
+++ src/main/java/com/vetrikos/inventory/system/entity/Order.java
@@ -8,12 +8,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -28,8 +31,9 @@ public class Order {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @OneToMany(mappedBy = "orderId")
-  private List<ItemListEntry> entries;
+  @OneToMany(mappedBy = "order")
+  @Builder.Default
+  private List<ItemListEntry> entries = new ArrayList<>();
 
   @ManyToOne
   @JoinColumn(name = "created_by")
@@ -43,4 +47,40 @@ public class Order {
   @JoinColumn(name = "to_warehouse_id")
   private Warehouse toWarehouse;
 
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    Class<?> oEffectiveClass = o instanceof HibernateProxy
+        ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+        : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy
+        ? ((HibernateProxy) this).getHibernateLazyInitializer()
+        .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
+      return false;
+    }
+    Order order = (Order) o;
+    return getId() != null && Objects.equals(getId(), order.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return getClass().hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return "Order{" +
+        "id=" + id +
+        ", entries=" + entries +
+        ", createdBy=" + createdBy +
+        ", fromWarehouse=" + fromWarehouse +
+        ", toWarehouse=" + toWarehouse +
+        '}';
+  }
 }
