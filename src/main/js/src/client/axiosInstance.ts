3f971effa5d163fa344ac48f4a485ef54234ca60
Filip Mlýnek <mlynekff@gmail.com>
2023-11-11 18:51:04 +0100

    Inject custom axios instance to clients

---
+++ src/main/js/src/client/axiosInstance.ts
@@ -0,0 +1,28 @@
+import axios, {AxiosInstance, AxiosRequestConfig} from "axios";
+import keycloak from "../keycloak/keycloak.ts";
+import {BASE_PATH} from "inventory-client-ts-axios/dist/esm/base";
+
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: BASE_PATH
+})
+
+axiosInstance.interceptors.request.use(
+    async (config: AxiosRequestConfig) => {
+
+      // Refresh the token if it has expired or has 10 or fewer seconds of validity remaining
+      await keycloak.updateToken(10);
+
+      const token = keycloak.token;
+
+      // @ts-ignore
+      config.headers.Authorization = `Bearer ${token}`;
+      // @ts-ignore
+      // config.headers["Access-Control-Allow-Origin"] = "*";
+
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+);
