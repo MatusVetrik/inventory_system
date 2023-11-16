15e47c54257ccf383e58233e1a0244f202644dce
Filip Mlýnek <mlynekff@gmail.com>
2023-11-16 18:25:12 +0100

    Check warehouse update capacity is greater than actual items capacity

--- src/main/java/com/vetrikos/inventory/system/service/WarehouseServiceImpl.java
+++ src/main/java/com/vetrikos/inventory/system/service/WarehouseServiceImpl.java
@@ -3,6 +3,7 @@ package com.vetrikos.inventory.system.service;
 import com.vetrikos.inventory.system.entity.Warehouse;
 import com.vetrikos.inventory.system.model.WarehouseRequestRestDTO;
 import com.vetrikos.inventory.system.model.WarehouseUpdateRequestRestDTO;
+import com.vetrikos.inventory.system.repository.ItemListEntryRepository;
 import com.vetrikos.inventory.system.repository.WarehouseRepository;
 import java.util.List;
 import lombok.NonNull;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class WarehouseServiceImpl implements WarehouseService {
 
   private final WarehouseRepository warehouseRepository;
+  private final ItemListEntryRepository itemListEntryRepository;
 
   @Override
   @NonNull
@@ -34,7 +36,7 @@ public class WarehouseServiceImpl implements WarehouseService {
   @NonNull
   @Transactional
   public Warehouse createWarehouse(WarehouseRequestRestDTO requestRestDTO) {
-  // TODO: potom asi pridat to warehousu usera co ho vytvoril
+    // TODO: potom asi pridat to warehousu usera co ho vytvoril
 
     Warehouse warehouse = Warehouse.builder()
         .name(requestRestDTO.getName())
@@ -51,6 +53,13 @@ public class WarehouseServiceImpl implements WarehouseService {
     Warehouse warehouse = warehouseRepository.findById(warehouseId)
         .orElseThrow(() -> new IllegalArgumentException(
             WarehouseService.warehouseNotFoundMessage(warehouseId)));
+
+    Long actualItemsCapacitySize = itemListEntryRepository.getActualItemsCapacitySize(warehouseId);
+
+    if (updateRequestRestDTO.getCapacity() < actualItemsCapacitySize) {
+      throw new IllegalArgumentException(
+          "New warehouse capacity can not be lower than current items capacity");
+    }
 
     warehouse.setCapacity(updateRequestRestDTO.getCapacity());
     warehouse.setName(updateRequestRestDTO.getName());
