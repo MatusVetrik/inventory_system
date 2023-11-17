0515f8e5d93dc9dbdef152e5142643f5f11627a9
Filip Mlýnek <xmlynek@stuba.sk>
2023-11-17 08:50:29 +0100

    Users api + dto warehouseName attribute (#25)

--- src/main/java/com/vetrikos/inventory/system/mapper/UserMapper.java
+++ src/main/java/com/vetrikos/inventory/system/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.vetrikos.inventory.system.mapper;
 import com.vetrikos.inventory.system.entity.User;
 import com.vetrikos.inventory.system.model.UserRestDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
 
 @Mapper
 public interface UserMapper {
 
+  @Mapping(target = "warehouseName", source = "warehouse.name", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
   UserRestDTO userToUserRestDTO(User user);
 }
