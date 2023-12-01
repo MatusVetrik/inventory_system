f56974254e8bddd34b588cdc9ca1ef66ab384ce1
siikmusic <xpavliks@stuba.sk>
2023-12-01 22:47:34 +0100

    Order persist fix

--- src/main/java/com/vetrikos/inventory/system/service/OrderServiceImpl.java
+++ src/main/java/com/vetrikos/inventory/system/service/OrderServiceImpl.java
@@ -8,10 +8,15 @@ import com.vetrikos.inventory.system.exception.ItemExceedsWarehouseCapacityExcep
 import com.vetrikos.inventory.system.model.OrderRestDTO;
 import com.vetrikos.inventory.system.repository.ItemListEntryRepository;
 import com.vetrikos.inventory.system.repository.OrderRepository;
+import com.vetrikos.inventory.system.repository.WarehouseRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -20,83 +25,134 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-    private final OrderRepository orderRepository;
-    private final ItemListEntryRepository itemListEntryRepository;
-    private final UserService userService;
-    private final ItemService itemService;
-    private final WarehouseService warehouseService;
 
-    @Override
-    @NonNull
-    public List<Order> findAllOrders() {
-        return orderRepository.findAll();
+  private final OrderRepository orderRepository;
+  private final ItemListEntryRepository itemListEntryRepository;
+  private final UserService userService;
+  private final ItemService itemService;
+  private final WarehouseService warehouseService;
+  private final WarehouseRepository warehouseRepository;
+  private static final Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Override
+  @NonNull
+  public List<Order> findAllOrders() {
+    return orderRepository.findAll();
+  }
+
+  @Override
+  @NonNull
+  @Transactional
+  public Order createOrder(OrderRestDTO requestRestDTO) {
+    String principalUuid = getPrincipalUuid();
+    Warehouse sourceWarehouse = warehouseService.findById(requestRestDTO.getSourceId());
+    Warehouse destinationWarehouse = warehouseService.findById(requestRestDTO.getDestinationId());
+    Item itemInSourceWarehouse = itemService.findItemInWarehouse(requestRestDTO.getSourceId(),
+        requestRestDTO.getItemId());
+    Long quantity = requestRestDTO.getQuantity();
+
+    validateEnoughItemForOrder(sourceWarehouse, itemInSourceWarehouse, quantity);
+    validateCapacityForOrder(destinationWarehouse, itemInSourceWarehouse.getSize(), quantity);
+
+    updateSourceWarehouseEntries(sourceWarehouse, itemInSourceWarehouse, quantity);
+    updateDestinationWarehouseEntries(destinationWarehouse, itemInSourceWarehouse, quantity);
+
+    Order order = Order.builder()
+        .createdBy(userService.findById(UUID.fromString(principalUuid)))
+        .item(itemInSourceWarehouse)
+        .quantity(requestRestDTO.getQuantity())
+        .fromWarehouse(warehouseService.findById(requestRestDTO.getSourceId()))
+        .toWarehouse(warehouseService.findById(requestRestDTO.getDestinationId()))
+        .build();
+
+    return orderRepository.save(order);  }
+
+  private void validateEnoughItemForOrder(Warehouse sourceWarehouse, Item itemInSourceWarehouse, Long quantity) {
+    List<ItemListEntry> existingItemSource = itemListEntryRepository.findItemListEntriesByWarehouseAndItem(
+        sourceWarehouse, itemInSourceWarehouse);
+
+    if (existingItemSource.isEmpty()) {
+      throw new IllegalStateException("No item found in source warehouse");
     }
 
-    @Override
-    @NonNull
-    @Transactional
-    public Order createOrder(OrderRestDTO requestRestDTO) {
-        String principalUuid = ((Jwt) SecurityContextHolder.getContext().getAuthentication()
-            .getPrincipal()).getSubject();
+    ItemListEntry itemListEntrySource = existingItemSource.get(0);
 
-        Warehouse sourceWarehouse = warehouseService.findById(requestRestDTO.getSourceId());
-        Warehouse destinationWarehouse = warehouseService.findById(requestRestDTO.getDestinationId());
-        Item itemInSourceWarehouse = itemService.findItemInWarehouse(requestRestDTO.getSourceId(), requestRestDTO.getItemId());
-        Long quantity = requestRestDTO.getQuantity();
-        Long itemSize = itemInSourceWarehouse.getSize();
-        Long itemsCapacity = itemSize * quantity;
-        Long currentCapacityDestination = calculateCurrentCapacity(destinationWarehouse);
-        //check if the capacity of the destination warehouse exceeds the capacity of the source warehouse
-        if (itemsCapacity + currentCapacityDestination > destinationWarehouse.getCapacity()) {
-            throw new ItemExceedsWarehouseCapacityException(
-                    (itemsCapacity + currentCapacityDestination) - destinationWarehouse.getCapacity());
-        }
-        List<ItemListEntry> existingItemSource = itemListEntryRepository.findItemListEntriesByWarehouseAndItem(
-                sourceWarehouse, itemInSourceWarehouse);
-        ItemListEntry itemListEntrySource = existingItemSource.get(0);
-        if (itemListEntrySource.getQuantity() - quantity < 0) { //if in source warehouse is not enough throw exception
-            throw new ItemExceedsWarehouseCapacityException(quantity - itemListEntrySource.getQuantity(), sourceWarehouse.getId());
-        }
-        //check if item already exists in the destination warehouse
-        List<ItemListEntry> existingItemDestination = itemListEntryRepository.findItemListEntriesByWarehouseAndItem(
-                destinationWarehouse, itemInSourceWarehouse);
-        if (existingItemDestination.isEmpty()) { //if no, create new item list entry
-            ItemListEntry newItemListEntry = new ItemListEntry();
-            newItemListEntry.setItem(itemInSourceWarehouse);
-            newItemListEntry.setQuantity(quantity);
-            itemListEntryRepository.save(newItemListEntry);
-        } else { //if yes, increase the quantity of the item in the destination warehouse
-            ItemListEntry itemListEntry = existingItemDestination.get(0);
-            itemListEntry.setQuantity(itemListEntry.getQuantity() + quantity);
-            itemListEntryRepository.save(itemListEntry);
-        }
-        //decrease quantity of itemListEntry in source warehouse
-        if (itemListEntrySource.getQuantity() - quantity == 0) { //if we send all items delete itemListEntry
-            itemListEntryRepository.delete(itemListEntrySource);
-        }
-        itemListEntrySource.setQuantity(itemListEntrySource.getQuantity() - quantity);
-        itemListEntryRepository.save(itemListEntrySource);
-
-        Order order = Order.builder()
-                .createdBy(userService.findById(UUID.fromString(principalUuid)))
-                .item(itemService.findItemInWarehouse(requestRestDTO.getSourceId(), requestRestDTO.getItemId()))
-                .quantity(requestRestDTO.getQuantity())
-                .fromWarehouse(warehouseService.findById(requestRestDTO.getSourceId()))
-                .toWarehouse(warehouseService.findById(requestRestDTO.getDestinationId()))
-                .build();
-        return orderRepository.save(order);
+    if (itemListEntrySource.getQuantity() < quantity) {
+      throw new ItemExceedsWarehouseCapacityException(quantity - itemListEntrySource.getQuantity(),
+          sourceWarehouse.getId());
     }
+  }
 
-    public Long calculateCurrentCapacity(Warehouse warehouse) {
-        Long currentCapacity = 0L;
+  private String getPrincipalUuid() {
+    Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    return jwt.getSubject();
+  }
 
-        List<ItemListEntry> itemList = warehouse.getEntries();
-        for (ItemListEntry entry : itemList) {
-            Long itemSize = entry.getItem().getSize();
-            Long itemQuantity = entry.getQuantity();
-            Long totalItemSize = itemSize * itemQuantity;
-            currentCapacity += totalItemSize;
-        }
-        return currentCapacity;
+  private void validateCapacityForOrder(Warehouse destinationWarehouse, Long itemSize,
+      Long quantity) {
+    Long itemsCapacity = itemSize * quantity;
+    Long currentCapacityDestination = calculateCurrentCapacity(destinationWarehouse);
+
+    if (itemsCapacity + currentCapacityDestination > destinationWarehouse.getCapacity()) {
+      throw new ItemExceedsWarehouseCapacityException(
+          (itemsCapacity + currentCapacityDestination) - destinationWarehouse.getCapacity());
     }
+  }
+
+  private void updateSourceWarehouseEntries(Warehouse sourceWarehouse, Item item, Long quantity) {
+    ItemListEntry itemListEntrySource = findItemListEntryInWarehouse(sourceWarehouse, item);
+
+    itemListEntrySource.setQuantity(itemListEntrySource.getQuantity() - quantity);
+    if (itemListEntrySource.getQuantity() == 0) {
+      removeAndDeleteItemListEntry(sourceWarehouse, itemListEntrySource);
+    } else {
+      itemListEntryRepository.save(itemListEntrySource);
+    }
+  }
+
+  private void updateDestinationWarehouseEntries(Warehouse destinationWarehouse, Item item,
+      Long quantity) {
+    List<ItemListEntry> existingItemDestination = itemListEntryRepository.findItemListEntriesByWarehouseAndItem(
+        destinationWarehouse, item);
+
+    if (existingItemDestination.isEmpty()) {
+      ItemListEntry newItemListEntry = ItemListEntry.builder().item(item)
+          .warehouse(destinationWarehouse).quantity(quantity).build();
+      itemListEntryRepository.save(newItemListEntry);
+    } else {
+      ItemListEntry itemListEntry = existingItemDestination.get(0);
+      itemListEntry.setQuantity(itemListEntry.getQuantity() + quantity);
+      itemListEntryRepository.save(itemListEntry);
+    }
+  }
+
+  private ItemListEntry findItemListEntryInWarehouse(Warehouse warehouse, Item item) {
+    return itemListEntryRepository.findItemListEntriesByWarehouseAndItem(warehouse, item)
+        .stream()
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("No item found in warehouse"));
+  }
+
+  private void removeAndDeleteItemListEntry(Warehouse warehouse, ItemListEntry itemListEntry) {
+    warehouse.getEntries().remove(itemListEntry);
+    itemListEntry.setWarehouse(null);
+    itemListEntryRepository.delete(itemListEntry);
+  }
+
+
+  public Long calculateCurrentCapacity(Warehouse warehouse) {
+    Long currentCapacity = 0L;
+
+    List<ItemListEntry> itemList = warehouse.getEntries();
+    for (ItemListEntry entry : itemList) {
+      Long itemSize = entry.getItem().getSize();
+      Long itemQuantity = entry.getQuantity();
+      Long totalItemSize = itemSize * itemQuantity;
+      currentCapacity += totalItemSize;
+    }
+    return currentCapacity;
+  }
+
 }
--- src/test/java/com/vetrikos/inventory/system/service/OrderServiceImplIT.java
+++ src/test/java/com/vetrikos/inventory/system/service/OrderServiceImplIT.java
@@ -20,8 +20,10 @@ import com.vetrikos.inventory.system.repository.OrderRepository;
 import com.vetrikos.inventory.system.repository.UserRepository;
 import com.vetrikos.inventory.system.repository.WarehouseRepository;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -34,166 +36,249 @@ import org.testcontainers.junit.jupiter.Container;
 @IntegrationTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class OrderServiceImplIT {
 
-    @Container
-    public static CustomPostgreSQLContainer postgreSQLContainer = CustomPostgreSQLContainer.getInstance();
+  @Container
+  public static CustomPostgreSQLContainer postgreSQLContainer = CustomPostgreSQLContainer.getInstance();
 
-    @Container
-    public static InventoryKeycloakContainer keycloakContainer = InventoryKeycloakContainer.getInstance();
+  @Container
+  public static InventoryKeycloakContainer keycloakContainer = InventoryKeycloakContainer.getInstance();
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private WarehouseRepository warehouseRepository;
+  @Autowired
+  private WarehouseRepository warehouseRepository;
 
-    @Autowired
-    private ItemListEntryRepository itemListEntryRepository;
+  @Autowired
+  private ItemListEntryRepository itemListEntryRepository;
 
-    @Autowired
-    private ItemRepository itemRepository;
+  @Autowired
+  private ItemRepository itemRepository;
 
-    @Autowired
-    private OrderRepository orderRepository;
+  @Autowired
+  private OrderRepository orderRepository;
 
-    @Autowired
-    private WarehouseServiceImpl warehouseService;
+  @Autowired
+  private WarehouseServiceImpl warehouseService;
 
-    @Autowired
-    private ItemServiceImpl itemService;
+  @Autowired
+  private ItemServiceImpl itemService;
 
-    @Autowired
-    private OrderServiceImpl orderService;
+  @Autowired
+  private OrderServiceImpl orderService;
 
-    private Item sampleItem;
-    private User sampleUser;
-    private Warehouse warehouse;
-    private Warehouse warehouse2;
-    private Warehouse warehouse3;
+  private Item sampleItem;
+  private User sampleUser;
+  private Warehouse warehouse;
+  private Warehouse warehouse2;
+  private Warehouse warehouse3;
 
-    @BeforeEach
-    void setUp() {
-        sampleItem = Item.builder()
-                .name("item1")
-                .size(50L)
-                .entries(new ArrayList<>())
-                .build();
-        sampleUser = User.builder()
-                .id(UUID.randomUUID())
-                .username("user")
-                .fullName("Sample User")
-                .build();
-        warehouse = Warehouse.builder()
-                .capacity(2800L)
-                .name("Warehouse")
-                .users(List.of(sampleUser))
-                .build();
-        warehouse2 = Warehouse.builder()
-                .capacity(2800L)
-                .name("Warehouse 2")
-                .users(List.of(sampleUser))
-                .build();
-        warehouse3 = Warehouse.builder()
-            .capacity(2000L)
-            .name("Warehouse 2")
-            .users(List.of(sampleUser))
-            .build();
-    }
+  @BeforeEach
+  void setUp() {
+    sampleItem = Item.builder()
+        .name("item1")
+        .size(50L)
+        .entries(new ArrayList<>())
+        .build();
+    sampleUser = User.builder()
+        .id(UUID.fromString("70c42715-ebd5-44bb-b1de-b74f44c6d110"))
+        .username("user")
+        .fullName("Sample User")
+        .build();
+    warehouse = Warehouse.builder()
+        .capacity(2800L)
+        .name("Warehouse")
+        .users(List.of(sampleUser))
+        .build();
+    warehouse2 = Warehouse.builder()
+        .capacity(2800L)
+        .name("Warehouse 2")
+        .users(List.of(sampleUser))
+        .build();
+    warehouse3 = Warehouse.builder()
+        .capacity(2000L)
+        .name("Warehouse 2")
+        .users(List.of(sampleUser))
+        .build();
+  }
 
-    @AfterEach
-    void tearDown() {
-        orderRepository.deleteAll();
-        userRepository.deleteAll();
-        itemRepository.deleteAll();
-        itemListEntryRepository.deleteAll();
-        warehouseRepository.deleteAll();
+  @AfterEach
+  void tearDown() {
+    orderRepository.deleteAll();
+    userRepository.deleteAll();
+    itemRepository.deleteAll();
+    itemListEntryRepository.deleteAll();
+    warehouseRepository.deleteAll();
 
-    }
+  }
 
-    @Test
-    @MockCustomJwtUser
-    void createOrder() {
-        warehouse = warehouseRepository.save(warehouse);
-        warehouse2 = warehouseRepository.save(warehouse2);
+  @Test
+  @MockCustomJwtUser
+  void createOrder() {
+    warehouse = warehouseRepository.save(warehouse);
+    warehouse2 = warehouseRepository.save(warehouse2);
 
-        sampleUser = userRepository.save(sampleUser);
+    sampleUser = userRepository.save(sampleUser);
 
-        ItemListEntry itemListEntry = ItemListEntry.builder()
-                .item(sampleItem)
-                .warehouse(warehouse)
-                .quantity(5L)
-                .build();
-        sampleItem.setEntries(Collections.singletonList(itemListEntry));
-        sampleItem = itemRepository.save(sampleItem);
-        ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
+    ItemListEntry itemListEntry = ItemListEntry.builder()
+        .item(sampleItem)
+        .warehouse(warehouse)
+        .quantity(5L)
+        .build();
+    sampleItem.setEntries(Collections.singletonList(itemListEntry));
+    sampleItem = itemRepository.save(sampleItem);
+    ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
 
-        Order order = Order.builder()
-                .createdBy(sampleUser)
-                .item(sampleItem)
-                .quantity(5L)
-                .fromWarehouse(warehouse)
-                .toWarehouse(warehouse2)
-                .build();
-        Order savedOrder = orderRepository.save(order);
+    Order order = Order.builder()
+        .createdBy(sampleUser)
+        .item(sampleItem)
+        .quantity(5L)
+        .fromWarehouse(warehouse)
+        .toWarehouse(warehouse2)
+        .build();
+    Order savedOrder = orderRepository.save(order);
 
-        List<Order> result = orderService.findAllOrders();
+    List<Order> result = orderService.findAllOrders();
 
-        assertThat(result).isNotEmpty()
-                .hasSize(1)
-                .containsExactlyInAnyOrder(savedOrder);
-    }
+    assertThat(result).isNotEmpty()
+        .hasSize(1)
+        .containsExactlyInAnyOrder(savedOrder);
+  }
 
-    @Test
-    @MockCustomJwtUser
-    void shouldNotCreateOrderNoItems() {
-        warehouse = warehouseRepository.save(warehouse);
-        warehouse3 = warehouseRepository.save(warehouse2);
-        sampleUser = userRepository.save(sampleUser);
+  @Test
+  @MockCustomJwtUser
+  void shouldNotCreateOrderNoItems() {
+    warehouse = warehouseRepository.save(warehouse);
+    warehouse3 = warehouseRepository.save(warehouse3);
+    sampleUser = userRepository.save(sampleUser);
 
-        ItemListEntry itemListEntry = ItemListEntry.builder()
-            .item(sampleItem)
-            .warehouse(warehouse)
-            .quantity(50L)
-            .build();
-        sampleItem.setEntries(Collections.singletonList(itemListEntry));
-        sampleItem = itemRepository.save(sampleItem);
-        ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
+    ItemListEntry itemListEntry = ItemListEntry.builder()
+        .item(sampleItem)
+        .warehouse(warehouse)
+        .quantity(50L)
+        .build();
+    sampleItem.setEntries(Collections.singletonList(itemListEntry));
+    sampleItem = itemRepository.save(sampleItem);
+    ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
 
-        OrderRestDTO orderRestDTO = new OrderRestDTO(sampleItem.getId(),
-            52L, warehouse.getId(), warehouse3.getId());
+    OrderRestDTO orderRestDTO = new OrderRestDTO(sampleItem.getId(),
+        52L, warehouse.getId(), warehouse3.getId());
 
-        Exception exception = assertThrows(ItemExceedsWarehouseCapacityException.class, () -> {
-            orderService.createOrder(orderRestDTO);
-        });
+    Exception exception = assertThrows(ItemExceedsWarehouseCapacityException.class,
+        () -> orderService.createOrder(orderRestDTO));
 
-        String expectedMessage = "2 items missing in warehouse with ID "+warehouse.getId();
-        String actualMessage = exception.getMessage();
-        Assertions.assertTrue(actualMessage.contains(expectedMessage));
-    }
-    @Test
-    @MockCustomJwtUser
-    void shouldNotCreateOrderNoCapacity() {
-        warehouse = warehouseRepository.save(warehouse);
-        warehouse3 = warehouseRepository.save(warehouse3);
-        sampleUser = userRepository.save(sampleUser);
+    String expectedMessage = "2 items missing in warehouse with ID " + warehouse.getId();
+    String actualMessage = exception.getMessage();
+    System.out.println(actualMessage);
+    Assertions.assertTrue(actualMessage.contains(expectedMessage));
+  }
 
-        ItemListEntry itemListEntry = ItemListEntry.builder()
-            .item(sampleItem)
-            .warehouse(warehouse)
-            .quantity(50L)
-            .build();
-        sampleItem.setEntries(Collections.singletonList(itemListEntry));
-        sampleItem = itemRepository.save(sampleItem);
-        ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
+  @Test
+  @MockCustomJwtUser
+  void shouldNotCreateOrderNoCapacity() {
+    warehouse = warehouseRepository.save(warehouse);
+    warehouse3 = warehouseRepository.save(warehouse3);
+    sampleUser = userRepository.save(sampleUser);
 
-        OrderRestDTO orderRestDTO = new OrderRestDTO(sampleItem.getId(),
-            48L, warehouse.getId(), warehouse3.getId());
+    ItemListEntry itemListEntry = ItemListEntry.builder()
+        .item(sampleItem)
+        .warehouse(warehouse)
+        .quantity(50L)
+        .build();
+    sampleItem.setEntries(Collections.singletonList(itemListEntry));
+    sampleItem = itemRepository.save(sampleItem);
+    ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
 
-        Exception exception = assertThrows(ItemExceedsWarehouseCapacityException.class, () -> {
-            orderService.createOrder(orderRestDTO);
-        });
+    OrderRestDTO orderRestDTO = new OrderRestDTO(sampleItem.getId(),
+        48L, warehouse.getId(), warehouse3.getId());
 
-        String expectedMessage = "Item exceeds warehouse capacity by "+400;
-        String actualMessage = exception.getMessage();
-        Assertions.assertTrue(actualMessage.contains(expectedMessage));
-    }
+    Exception exception = assertThrows(ItemExceedsWarehouseCapacityException.class,
+        () -> orderService.createOrder(orderRestDTO));
+
+    String expectedMessage = "Item exceeds warehouse capacity by " + 400;
+    String actualMessage = exception.getMessage();
+    Assertions.assertTrue(actualMessage.contains(expectedMessage));
+  }
+
+  @Test
+  @MockCustomJwtUser
+  void createOrderWith0Quantity() {
+    warehouse = warehouseRepository.save(warehouse);
+    warehouse2 = warehouseRepository.save(warehouse2);
+    sampleUser = userRepository.save(sampleUser);
+
+    ItemListEntry itemListEntry = ItemListEntry.builder()
+        .item(sampleItem)
+        .warehouse(warehouse)
+        .quantity(5L)
+        .build();
+    sampleItem.setEntries(Collections.singletonList(itemListEntry));
+    sampleItem = itemRepository.save(sampleItem);
+    ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
+
+    OrderRestDTO orderRestDTO = new OrderRestDTO(sampleItem.getId(),
+        5L, warehouse.getId(), warehouse2.getId());
+
+    Order createdOrder = orderService.createOrder(orderRestDTO);
+
+    Optional<Order> orderOptional = orderRepository.findById(createdOrder.getId());
+    Assertions.assertTrue(orderOptional.isPresent());
+    Optional<Warehouse> warehouseOptional = warehouseRepository.findById(warehouse.getId());
+    Optional<Warehouse> warehouse2Optional = warehouseRepository.findById(warehouse2.getId());
+    Assertions.assertTrue(warehouse2Optional.isPresent());
+    Assertions.assertTrue(warehouseOptional.isPresent());
+    warehouse = warehouseOptional.get();
+    warehouse2 = warehouse2Optional.get();
+    assertThat(warehouse.getEntries().size()).isEqualTo(0);
+    assertThat(warehouse2.getEntries().size()).isEqualTo(1);
+    List<Order> result = orderService.findAllOrders();
+
+    assertThat(result).isNotEmpty()
+        .hasSize(1);
+  }
+
+  @Test
+  @MockCustomJwtUser
+  void createOrderItemInOtherWarehouse() {
+    warehouse = warehouseRepository.save(warehouse);
+    warehouse2 = warehouseRepository.save(warehouse2);
+    sampleUser = userRepository.save(sampleUser);
+    Long quantity = 5L;
+    ItemListEntry itemListEntry = ItemListEntry.builder()
+        .item(sampleItem)
+        .warehouse(warehouse)
+        .quantity(quantity)
+        .build();
+    ItemListEntry itemListEntry2 = ItemListEntry.builder()
+        .item(sampleItem)
+        .warehouse(warehouse2)
+        .quantity(quantity)
+        .build();
+
+    sampleItem.setEntries(new ArrayList<>(Arrays.asList(itemListEntry, itemListEntry2)));
+    sampleItem = itemRepository.save(sampleItem);
+    ItemListEntry savedItemListEntry = itemListEntryRepository.save(itemListEntry);
+    ItemListEntry savedItemListEntry2 = itemListEntryRepository.save(itemListEntry2);
+
+    OrderRestDTO orderRestDTO = new OrderRestDTO(sampleItem.getId(),
+        5L, warehouse.getId(), warehouse2.getId());
+
+    Order createdOrder = orderService.createOrder(orderRestDTO);
+
+    Optional<Order> orderOptional = orderRepository.findById(createdOrder.getId());
+    Assertions.assertTrue(orderOptional.isPresent());
+    Optional<Warehouse> warehouseOptional = warehouseRepository.findById(warehouse.getId());
+    Optional<Warehouse> warehouse2Optional = warehouseRepository.findById(warehouse2.getId());
+    Assertions.assertTrue(warehouse2Optional.isPresent());
+    Assertions.assertTrue(warehouseOptional.isPresent());
+    warehouse = warehouseOptional.get();
+    warehouse2 = warehouse2Optional.get();
+    assertThat(warehouse.getEntries().size()).isEqualTo(0);
+    assertThat(warehouse2.getEntries().size()).isEqualTo(1);
+    assertThat(warehouse2.getEntries().get(0).getQuantity()).isEqualTo(quantity * 2);
+    List<Order> result = orderService.findAllOrders();
+
+    assertThat(result).isNotEmpty()
+        .hasSize(1);
+  }
+
 }
